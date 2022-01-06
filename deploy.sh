c4e2377084559ef4c97539cbde8e44afd4cc305a
Maxime Bouillot <maxbouillot@gmail.com>
2022-01-06 23:29:55 +0100

    Deploy script copies the Gemini blog.

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
+
+# Web
 hexo clean &&
     hexo generate && 
     rm -v public/big_files/* &&
     ln -s $PWD/big_files/* $PWD/public/big_files/ &&
-    sudo /bin/rm -rv /srv/data/www/blog/* &&
+    sudo /bin/rm -rfv /srv/data/www/blog/* &&
     sudo cp -rv public/* /srv/data/www/blog/ &&
     echo Done
+
+# Gemini
+sudo /bin/rm -rfv /srv/data/gemini/content &&
+    sudo mkdir -p /srv/data/gemini/content &&
+    sudo cp -rv gemini /srv/data/gemini/content/bobignou.red &&
+    sudo ln -s /srv/data/gemini/content/bobignou.red /srv/data/gemini/content/gemini.bobignou.red
 
