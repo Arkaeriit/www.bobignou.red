bc144760152b639cfaaf925aee941cc5e24618d6
Maxime Bouillot <maxime.bouillot@withings.com>
2023-01-13 15:00:09 +0100

    Cleanup gemini deploy and add post index.

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-HEXO='./node_modules/hexo/bin/hexo'
-
 # Web
 ./renderer.py &&
     sudo /bin/rm -rfv /srv/data/www/blog/* &&
     sudo cp -rv public/* /srv/data/www/blog/ &&
     echo Done
 
-# Gemini atom
-sed 's|https://|gemini://|g; s:\.html:.gmi:g; s:html:text:g;' < public/atom.xml > gemini/atom.xml
-
 # Gemini
 sudo /bin/rm -rfv /srv/data/gemini/content &&
     sudo mkdir -p /srv/data/gemini/content &&
     sudo cp -rv gemini /srv/data/gemini/content/bobignou.red &&
     sudo ln -s /srv/data/gemini/content/bobignou.red /srv/data/gemini/content/gemini.bobignou.red
+
+# Gemini atom and post list
+sed 's|https://|gemini://|g; s:\.html:.gmi:g; s:html:text:g;' < public/atom.xml | sudo tee  /srv/data/gemini/content/bobignou.red/atom.xml
+printf '# Posts\n\n' | sudo tee /srv/data/gemini/content/bobignou.red/posts/index.gmi
+grep "=> posts" < gemini/index.gmi | sed 's:=> posts/:=> :' | sudo tee -a /srv/data/gemini/content/bobignou.red/posts/index.gmi
 
